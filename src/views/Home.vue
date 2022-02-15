b750f6e2db98a5b2244d76aaa9f5fdcad9cd8413
Marc Akbar <marc.akbar@heska.com>
2022-02-14 18:28:29 -0700

    WIP work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Greeting from "components/Greeting/Greeting.vue";
+import Work from "components/Work/Work.vue";
 </script>
 
 <template>
   <Greeting :darkMode="darkMode" />
+  <Work :darkMode="darkMode" />
 </template>
