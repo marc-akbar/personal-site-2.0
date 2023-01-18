12cab4271770c9e2bf27a1bfc39426474d0d4d77
Marc Akbar <marc.akbar@heska.com>
2023-01-18 12:22:29 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 # abort on errors
 set -e
 # build
-npm run-script build
+vite build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 git push -f git@github.com:marc-akbar/personal-site-2.0.git master:gh-pages
 cd -
